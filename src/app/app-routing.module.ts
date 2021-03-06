969622134540474904d3f2a29bb74f1e863162ff
Jake Mangan <jakemangan1@hotmail.co.uk>
2021-03-06 23:31:50 +0000

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/pages/home/home.component';
+import {SearchResultComponent} from './components/pages/search-result/search-result.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: "search/:ticker",
+    component: SearchResultComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
